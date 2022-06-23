b39e33df85fab02d51e93d1a0715d0c75085a2fc
Daniel Figueiredo Pereira <danielfp.pc@hotmail.com>
2022-06-23 16:32:07 -0300

    Add pick and place And another conveyor

--- Backups/Controller_Pintura_220622-234720/HOME/ipsdata/node1/autoexec.bat
+++
@@ -1,5 +0,0 @@
-createicitopo nodes
-cfg load config timeout 30
-!call param
-ips A1BrushTabLim ws Enable 0
-ips A1MainV ws SetPoint 1
---
+++ Backups/Controller_Pintura_220623-163024/HOME/ipsdata/node1/sysdump.bat
@@ -0,0 +1,20 @@
+rtc
+rtc stat
+task
+ips task
+can dump
+state
+mem map p
+mem wm
+mem dump 0x000
+mem dump 0x070
+mem dump 0x0f0
+mem dump 0x170
+mem dump 0x200
+mem dump 0x280
+mem dump 0x380
+mem dump 0x500
+mem dump 0x580
+mem dump 0x2200
+mem wm
+rtc
